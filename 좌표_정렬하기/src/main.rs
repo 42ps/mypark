b1ed21a786a8b2018ef366cd1958dfb836987166
42mypark <skyttl34@gmail.com>
2024-02-07 14:17:23 +0900

    좌표 정렬하기

---
+++ 좌표_정렬하기/src/main.rs
@@ -0,0 +1,48 @@
+// INPUT
+// 5
+// 3 4
+// 1 1
+// 1 -1
+// 2 2
+// 3 3
+
+// OUTPUT
+// 1 -1
+// 1 1
+// 2 2
+// 3 3
+// 3 4
+
+use std::fmt::Write;
+use std::io::Read;
+
+fn main() {
+	let mut points = input().expect("coordinates");
+
+	points.sort();
+
+	let mut output = String::new();
+
+	for p in points {
+		let _ = writeln!(output, "{} {}", p.0, p.1);
+	}
+
+	print!("{output}");
+}
+
+fn input() -> Result<Vec<(isize, isize)>, Box<dyn std::error::Error>> {
+	let mut buf = String::new();
+
+	std::io::stdin().read_to_string(&mut buf)?;
+
+	let mut iter = buf.split_ascii_whitespace().skip(1);
+	let mut v = Vec::new();
+
+	while let [Some(x), Some(y)] =
+		[(); 2].map(|_| iter.next().and_then(|x| x.parse::<isize>().ok()))
+	{
+		v.push((x, y))
+	}
+
+	Ok(v)
+}
